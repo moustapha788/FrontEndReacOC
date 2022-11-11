0872f8614dffe2f9d4ad3f20ff56906ea6457604
moustapha788 <gayemoustaphsmg97@gmail.com>
2022-11-11 08:35:59 +0000

    du inline-style

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,8 +1,16 @@
 import "../styles/Banner.css"
 
 function Banner() {
+    /*className='lmj-banner'*/
     return (
-        <div className='lmj-banner'>
+        <div style={
+            {
+                color:"black",
+                textAlign:"right",
+                padding:32/* "32px" */,
+                borderBottom:"solid 3px black"
+            }
+        }>
             <h1>La maison jungle</h1>
         </div>
     );
